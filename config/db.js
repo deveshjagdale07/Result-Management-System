01cf90bcc28219e5a149ba7e694e90960c93279f
deveshjagdale07 <163432910+deveshjagdale07@users.noreply.github.com>
2024-11-04 16:25:49 +0530

    Update student controller to support PostgreSQL

--- config/db.js
+++ config/db.js
@@ -3,12 +3,8 @@ require('dotenv').config(); // Load environment variables from .env file
 
 // Create a connection pool to PostgreSQL using environment variables
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5432, // Use 5432 as default for PostgreSQL
-});
+  connectionString: process.env.POSTGRES_URL,
+})
 
 // Test the connection pool
 pool.connect((err, client, release) => {
